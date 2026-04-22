6399184d1b0f48401abae96363580da06a76f488
Le Phuoc Loc <lephuocloc00z12@gmail.com>
2026-04-22 13:36:04 +0700

    bai 1

---
+++ bai1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PRODUCT (
+	ID INT PRIMARY KEY auto_increment,
+    ProductName varchar(100) not null,
+    Price DECIMAL(18,4) not null,
+    Description varchar(255) not null
+);
+
+-- thay thế description kiểu text thành varchar vì text chỉ để lưu trữ nội dung là bài viết dài, nên sẽ gây tốn dung lượng hơn
